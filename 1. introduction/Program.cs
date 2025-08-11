ad2c10e997d37edeb9203bd8ebe70cb5b34a960c
Amanbig <amanpreetsinghjhiwant@outlook.com>
2025-08-11 12:29:01 +0530

    added introduction

---
+++ 1. introduction/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Introduction {
+    class Program {
+        static void Main(string[] args){
+            // THis is single line comment
+            /*
+            This is a multi line comment
+            */
+            Console.WriteLine("Hello, World!");
+            Console.Write("Press any key to exit...");
+        }
+    }
+}
