43f054e5bd8d34d40fb827729bce8742ed1e28be
Amanbig <amanpreetsinghjhiwant@outlook.com>
2025-08-11 12:45:20 +0530

    added user input

---
+++ 4. User Input/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace UserInput{
+    class Program{
+        static void Main(string[] args){
+            Console.WriteLine("Please enter your name:");
+            string name = Console.ReadLine();
+
+            Console.WriteLine("Here is your name:"+name);
+
+            int age = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("You are" + age + "years old.");
+        }
+    }
+}
