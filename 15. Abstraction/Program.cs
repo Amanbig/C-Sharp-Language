91db75d55f625a915ba2e079474167fea9084483
Amanpreet <rogstrixacer@outlook.com>
2025-08-11 22:44:17 +0530

    added abstraction

---
+++ 15. Abstraction/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace AbstractionExample{
+    abstract class Animal{
+        public abstract void Speak();
+    }
+
+    class Dog : Animal{
+        public override void Speak(){
+            Console.WriteLine("Dog barks");
+        }
+    }
+
+    class Program{
+        static void Main(string[] args){
+            Animal myDog = new Dog();
+            myDog.Speak();
+        }
+    }
+}
