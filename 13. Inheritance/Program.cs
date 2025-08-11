480caf6f6c5645eec1bb2c0a30b5c8c5aeb40c4a
Amanpreet <rogstrixacer@outlook.com>
2025-08-11 22:38:17 +0530

    added inheritance

---
+++ 13. Inheritance/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace InheritanceExample{
+    // sealed class Animal{
+    //     public void Speak(){
+    //         Console.WriteLine("Animal speaks");
+    //     }
+    // }
+    class Animal{
+        public void Speak(){
+            Console.WriteLine("Animal speaks");
+        }
+    }
+
+    class Dog : Animal{
+        public void Bark(){
+            Console.WriteLine("Dog barks");
+        }
+    }
+
+    class Program{
+        static void Main(string[] args){
+            Dog dog = new Dog();
+            dog.Speak();
+            dog.Bark();
+        }
+    }
+}
