79ec8db13142a14e7444b08604321a5907d1a26f
Amanbig <amanpreetsinghjhiwant@outlook.com>
2025-08-11 12:30:10 +0530

    added variables

---
+++ 2. Variables/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Variables{
+    class Program{
+        static void Main(string[] args){
+            Console.WriteLine("This is a variables example in C#");
+            int age = 25;
+            string name = "John Doe";
+            double height  = 5.9;
+            bool isEmployed = true;
+            char initial = 'J';
+            float salary = 50000.50f;
+            long population = 7800000000L;
+            Console.WriteLine($"Name: {name}, Age: {age}, Height: {height}, Employed: {isEmployed}, Initial: {initial}, Salary: {salary}, Population: {population}");
+
+            const int maxScore = 100;
+            Console.WriteLine($"Maximum Score: {maxScore}");
+    }
+}
+}
