41acdaa3b7a21defee3eefc759dadf60626cab33
Amanpreet <rogstrixacer@outlook.com>
2025-08-11 22:53:22 +0530

    added exceptions

---
+++ 19. Exceptions/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ExceptionsExample{
+    class Program{
+        static void Main(string[] args){
+            try{
+                int[] numbers = {1, 2, 3};
+                Console.WriteLine(numbers[5]);
+            }catch(IndexOutOfRangeException e){
+                Console.WriteLine("Error: " + e.Message);
+            }
+            finally
+{
+  Console.WriteLine("The 'try catch' is finished.");
+}
+        }
+    }
+}
