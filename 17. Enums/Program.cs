81b839bde96cd70e05a214a58458e7e57069546e
Amanpreet <rogstrixacer@outlook.com>
2025-08-11 22:48:10 +0530

    added enums

---
+++ 17. Enums/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace EnumsExample{
+    enum Days{
+        Sunday,
+        Monday,
+        Tuesday,
+        Wednesday,
+        Thursday,
+        Friday,
+        Saturday
+    }
+
+    class Program{
+        static void Main(string[] args){
+            Days today = Days.Monday;
+            Console.WriteLine("Today is " + today);
+        }
+    }
+}
