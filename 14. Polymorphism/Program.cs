8b2fe73d0e930309e1fbfc58ded702bd4ff3eb50
Amanpreet <rogstrixacer@outlook.com>
2025-08-11 22:41:53 +0530

    added polymorphism

---
+++ 14. Polymorphism/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace PolymorhpismExample{
+
+    class Animal {
+        public virtual void Speak() {
+            Console.WriteLine("Animal speaks");
+        }
+    }
+
+    class Dog : Animal {
+        public override void Speak() {
+            Console.WriteLine("Dog barks");
+        }
+    }
+
+    class Program {
+        static void Main(string[] args) {
+            Animal myAnimal = new Animal();
+            myAnimal.Speak();
+
+            Dog myDog = new Dog();
+            myDog.Speak();
+        }
+    }
+}
