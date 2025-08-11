0dee46f58fd6a069c91150d8b5018fa88a526b73
Amanpreet <rogstrixacer@outlook.com>
2025-08-11 22:24:52 +0530

    added methods

---
+++ 11. Methods/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Methods{
+    class Program{
+        static void PrintMessage(string message){
+            Console.WriteLine(message);
+        }
+        static void Prints(string message = "Hello"){
+            Console.WriteLine(message);
+        }
+        static int Increments(int number){
+            number++;
+            return number;
+        }
+        static void  Temp(string child1, string child2){
+            Console.WriteLine($"Child 1: {child1}, Child 2: {child2}"); 
+        }
+        static void PlusMethod(int x, int y){
+            Console.WriteLine($"Sum: {x + y}");
+        }
+        static void PlusMethod(double x, double y){
+            Console.WriteLine($"Sum: {x + y}");
+        }
+        static void Main(string[] args){
+            PrintMessage("Hello, World!");
+            Prints();
+            int result = Increments(5);
+            Console.WriteLine("Incremented value is: " + result);
+            Temp(child1:"Hello",child2:"Welcome");
+            PlusMethod(5, 10);
+            PlusMethod(5.5, 10.5);
+        }
+    }
+}
