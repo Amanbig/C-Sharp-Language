e378beea57e891cd336f5a7b0b00e4321a62c918
Amanpreet <rogstrixacer@outlook.com>
2025-08-11 22:51:04 +0530

    added files

---
+++ 18. Files/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.IO;
+
+namespace FilesExample{
+    class Program{
+        static void Main(string[] args){
+            string filePath = "example.txt";
+            File.WriteAllText(filePath, "hello world!");
+
+            string readText = File.ReadAllText(filePath);  // Read the contents of the file
+            Console.WriteLine(readText);  // Output the content
+        }
+    }
+}
