4df3eba94a2480ca24010c5ae9ea11d71d29e0cd
Amanpreet <rogstrixacer@outlook.com>
2025-08-11 20:33:29 +0530

    added math operations

---
+++ 6. Math/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace MathOperations{
+    class Program{
+        static void Main(string[] args){
+            Console.WriteLine("This is the program for math operations");
+            Console.WriteLine(Math.Max(2,3));
+            Console.WriteLine(Math.Min(3,4));
+            Console.WriteLine(Math.Sqrt(4));
+            Console.WriteLine(Math.Pow(2,3));
+            Console.WriteLine(Math.Abs(-5));
+            Console.WriteLine(Math.Round(4.5454));
+        }
+    }
+}
