cadc928639371deb95599f6957a5495d542d13fb
Amanpreet <rogstrixacer@outlook.com>
2025-08-11 22:46:33 +0530

    added interfaces

---
+++ 16. Interfaces/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace InterfacesExample{
+    interface IAnimal{
+        void Speak();
+    }
+
+    class Dog : IAnimal{
+        public void Speak(){
+            Console.WriteLine("Dog barks");
+        }
+    }
+
+    class Program{
+        static void Main(string[] args){
+            IAnimal myDog = new Dog();
+            myDog.Speak();
+        }
+    }
+}
